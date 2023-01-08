84d031489de99d136f6b1fff7e205c593d0c0c1c
devferx <ferkki@hotmail.com>
2023-01-07 20:34:25 -0400

    :sparkles: [ADD] Copy text from btn

--- src/components/SuccessCard.tsx
+++ src/components/SuccessCard.tsx
@@ -1,13 +1,36 @@
+import { useState } from "react";
+
 interface SuccessCardProps {
   imgUrl: string;
 }
 
 export const SuccessCard = ({ imgUrl }: SuccessCardProps) => {
+  const [message, setMessage] = useState<string>();
+
+  const onClick = () => {
+    navigator.clipboard.writeText(imgUrl).then(() => {
+      setMessage("Link copied to clipboard");
+      setTimeout(() => {
+        setMessage("");
+      }, 2000);
+    });
+  };
   return (
     <div className="card">
       <h2 className="card__title">🎉 Uploaded successfully</h2>
       <img className="card__img" src={imgUrl} alt="uploaded image" />
-      <input type="text" readOnly value={imgUrl} />
+      <div className="card__link">
+        <input
+          className="card__link__input"
+          type="text"
+          readOnly
+          value={imgUrl}
+        />
+        <button className="card__btn" onClick={onClick}>
+          Copy Link
+        </button>
+      </div>
+      {message && <p className="card__label">{message}</p>}
     </div>
   );
 };
